1629a8f9cdfb65ff3ad8d8ae526b680de2246029
CrystalKhadka <121247519+CrystalKhadka@users.noreply.github.com>
2023-06-16 14:47:30 +0545

    Added img in database

--- src/main/java/com/ecommerce/ecommerce/controller/ItemController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ItemController.java
@@ -12,7 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -32,16 +34,29 @@ public class ItemController {
 
 
     @PostMapping("/save")
-    public String saveItem(ItemDto itemDto, @RequestParam("itemImage") MultipartFile itemImage) throws IOException {
+    public String saveItem(ItemDto itemDto) throws IOException {
 
 
-        byte[] imageBytes = itemImage.getBytes();
+
         // Call your itemService to save the itemDto object
-        itemService.addItem(itemDto,imageBytes);
+        itemService.addItem(itemDto);
 
         return "redirect:/seller/item/add";
     }
 
+    public String getImageBase64(String fileName) {
+        String filePath = System.getProperty("user.dir") + "/item_img/";
+        File file = new File(filePath + fileName);
+        byte[] bytes = new byte[0];
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        String base64 = Base64.getEncoder().encodeToString(bytes);
+        return base64;
+    }
 
 
 }
--- src/main/java/com/ecommerce/ecommerce/service/CartService.java
+++ src/main/java/com/ecommerce/ecommerce/service/CartService.java
@@ -1,8 +1,13 @@
 package com.ecommerce.ecommerce.service;
 
 import com.ecommerce.ecommerce.dto.CartDto;
+import com.ecommerce.ecommerce.entity.Cart;
+
+import java.util.List;
 
 public interface CartService {
 
     void addCart(CartDto cartDto);
+
+    List<Cart> getDataByUserId(int UserId);
 }
