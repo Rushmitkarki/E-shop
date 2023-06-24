b20e1862a30ec517d736fb958cdcb0c1a3693b3c
CrystalKhadka <121247519+CrystalKhadka@users.noreply.github.com>
2023-06-24 11:06:29 +0545

    Testing check

--- src/test/java/com/ecommerce/ecommerce/service/impl/UserServiceImplTest.java
+++ src/test/java/com/ecommerce/ecommerce/service/impl/UserServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.ecommerce.ecommerce.service.impl;
 
-
 import com.ecommerce.ecommerce.dto.UserDto;
 import com.ecommerce.ecommerce.entity.User;
 import com.ecommerce.ecommerce.repo.UserRepo;
@@ -14,26 +13,26 @@ import org.springframework.test.annotation.Rollback;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UserServiceImplTest{
+public class UserServiceImplTest {
     @Autowired
     private UserRepo userRepo;
 
-//    JUnit to register User
+    // JUnit to register User
     @Test
     @Order(1)
 
     @Rollback(value = false)
     public void registerUser() {
-       User user= User.builder()
-               .fname("Test")
+        User user = User.builder()
+                .fname("Test")
                 .lname("User")
-                .email("g@gmail.com")
+                .email("s@gmail.com")
                 .password("test")
                 .role("USER")
                 .status("ACTIVE")
                 .sq("Momo")
                 .build();
-         userRepo.save(user);
+        userRepo.save(user);
 
         Assertions.assertThat(user.getUserId()).isGreaterThan(0);
 
@@ -42,10 +41,9 @@ public class UserServiceImplTest{
     @Test
     @Order(2)
     void getByEmail() {
-        String email="t@gmail.com";
-        User user=userRepo.findByEmail(email).get();
+        String email = "t@gmail.com";
+        User user = userRepo.findByEmail(email).get();
         Assertions.assertThat(user.getEmail()).isEqualTo(email);
     }
-
 
 }
