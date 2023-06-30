a2aef81844a87311ce1e40a3b06161e621d29c1e
slesha45 <dahalslesha@gmail.com>
2023-06-30 14:36:25 +0545

    Some changes

--- src/main/java/com/ecommerce/ecommerce/entity/Notification.java
+++ src/main/java/com/ecommerce/ecommerce/entity/Notification.java
@@ -1,2 +1,32 @@
-package com.ecommerce.ecommerce.entity;public class Notification {
+package com.ecommerce.ecommerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "notify")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+    @Id
+    @SequenceGenerator(sequenceName = "notify_id_generator" ,name="notify_seq_generator",allocationSize = 1)
+    @GeneratedValue(generator = "notify_seq_generator",strategy = GenerationType.SEQUENCE)
+    private int notifyId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name="notify_content")
+    private String notifyContent;
+
+
+
+
 }
