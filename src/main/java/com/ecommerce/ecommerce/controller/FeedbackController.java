fc088478a083ab73a1ef691e6d61b90e17062901
CrystalKhadka <121247519+CrystalKhadka@users.noreply.github.com>
2023-07-02 16:55:56 +0545

    Completed Feedback

---
+++ src/main/java/com/ecommerce/ecommerce/controller/FeedbackController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.ecommerce.controller;
+
+import com.ecommerce.ecommerce.dto.FeedbackDto;
+import com.ecommerce.ecommerce.service.FeedbackService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/feedback")
+public class FeedbackController {
+    private  final FeedbackService feedbackService;
+    @RequestMapping("")
+    public String feedback(){
+        return "Feedback";
+    }
+
+    @PostMapping("/save")
+    public String saveFeedback(@Valid FeedbackDto feedbackDto){
+        feedbackService.saveFeedback(feedbackDto);
+        return "redirect:/feedback";
+    }
+}
