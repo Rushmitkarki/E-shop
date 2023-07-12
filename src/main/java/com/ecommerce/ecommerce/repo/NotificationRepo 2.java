d945e75f9fe7e7f42f2d667ec0ecb7d5ad3768bb
CrystalKhadka <121247519+CrystalKhadka@users.noreply.github.com>
2023-07-12 12:12:59 +0545

    fixes

--- src/main/java/com/ecommerce/ecommerce/repo/NotificationRepo 2.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.ecommerce.repo;
-
-import com.ecommerce.ecommerce.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepo extends JpaRepository<Notification,Integer> {
-}
